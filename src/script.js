958f8b9b95ad3dd37bc8b49f5c1a2de13138ff71
notGamgee <139826546+notGamgee@users.noreply.github.com>
2023-10-17 04:29:48 -0400

    icon
    wrote function to change the weather icon according to weather conditions

--- src/script.js
+++ src/script.js
@@ -12,10 +12,40 @@ function displayTemperature(response) {
   wind.innerHTML = Math.round(response.data.wind.speed);
   let adjusted = document.querySelector("#adjusted-temp");
   adjusted.innerHTML = Math.round(response.data.main.feels_like);
+
+  function displayIcon() {
+    let condition = document.querySelector(".condition").innerHTML;
+    let icon = document.querySelector(".main-icon i");
+
+    if (condition.includes("clear sky")) {
+      icon.className = "fa-solid fa-sun fa-4x";
+    } else {
+      if (condition.includes("clouds")) {
+        icon.className = "fa-solid fa-cloud fa-4x";
+      } else {
+        if (condition.includes("rain")) {
+          icon.className = "fa-solid fa-cloud-rain fa-4x";
+        } else {
+          if (condition.includes("thunderstorm")) {
+            icon.className = "fa-solid fa-cloud-bolt fa-4x";
+          } else {
+            if (condition.includes("snow")) {
+              icon.className = "fa-solid fa-snowflake fa-4x";
+            } else {
+              if (condition.includes("mist")) {
+                icon.className = "fa-solid fa-smog fa-4x";
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  displayIcon();
 }
+
 let apiKey = "7059cb165caa3316bff682d263a01b1e";
 let city = document.querySelector("h1").innerHTML;
-
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
@@ -42,6 +72,6 @@ let currentDate = now.getDate();
 let currentTime = `${now.getHours()}:${now.getMinutes()}`;
 
 let weekDay = document.querySelector("#day-of-week");
-weekDay.innerHTML = `${currentDay}`;
+weekDay.innerHTML = `${currentDay},`;
 let timeStamp = document.querySelector("#date-and-time");
-timeStamp.innerHTML = `${currentMonth}, ${currentDate} ${currentTime}`;
+timeStamp.innerHTML = `${currentMonth} ${currentDate} ${currentTime}`;
